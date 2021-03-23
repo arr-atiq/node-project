8c8fd2d091b7d200895d2aa383aea440d33bc915
aar-atiq <aar.msdeveloper01@gmail.com>
2021-03-24 03:46:25 +0600

    dynamic api, api parameter, access params, access query

--- index.js
+++ index.js
@@ -7,5 +7,11 @@ app.get('/', (req, res) =>{
 app.get('/fruits/banana', (req, res)=>{
     res.send({fruit: 'banana', quantity: 2000, price: 5000});
 })
+const users = ['atiq', 'john', 'alberto', 'david', 'tisco', 'costa']
+app.get('/users/:id', (req, res) =>{
+    const userId = req.params.id;
+    const name = users[userId];
+    res.send({userId,name});
+})
 
 app.listen(3000, () => console.log("Listening to port 3000"));
