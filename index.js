26a7aa3348eba9cf99ffb73f4d93296cd1de3f85
aar-atiq <aar.msdeveloper01@gmail.com>
2021-03-23 21:17:09 +0600

    install nodemon, api return object, create multiple api

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 
 app.get('/', (req, res) =>{
     res.send("Thank you for calling me");
+});
+app.get('/fruits/banana', (req, res)=>{
+    res.send({fruit: 'banana', quantity: 2000, price: 5000});
 })
 
 app.listen(3000, () => console.log("Listening to port 3000"));
