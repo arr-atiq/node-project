27a0acbd77a45d67a02b19ebf9ef9e1fc2786eb4
aar-atiq <aar.msdeveloper01@gmail.com>
2021-03-24 12:15:33 +0600

    added get method and post method

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
+
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) =>{
     res.send("Thank you for calling me");
@@ -15,6 +18,13 @@ app.get('/users/:id', (req, res) =>{
     const userId = req.params.id;
     const name = users[userId];
     res.send({userId,name});
+});
+
+app.post('/addUser', (req, res) =>{
+    const user = req.body;
+    user.id = 55;
+    res.send(user);
+    // res.send(req.body);
 })
 
 app.listen(3000, () => console.log("Listening to port 3000"));
